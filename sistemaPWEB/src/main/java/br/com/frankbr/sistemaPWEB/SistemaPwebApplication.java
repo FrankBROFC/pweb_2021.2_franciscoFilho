b774295a17c8c8aa3fa8814472856f74851dbf1e
Francisco Filho <fcofilho0809@gmail.com>
2022-05-03 13:04:37 -0300

    feat: Criação inicial do projeto.

---
+++ sistemaPWEB/src/main/java/br/com/frankbr/sistemaPWEB/SistemaPwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.frankbr.sistemaPWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPwebApplication.class, args);
+	}
+
+}
---
+++ sistemaPWEB/src/test/java/br/com/frankbr/sistemaPWEB/SistemaPwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frankbr.sistemaPWEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
